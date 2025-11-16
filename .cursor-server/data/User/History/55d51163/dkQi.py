9aef6394474b0e940f612bc5fd6c1e71d551cbc9
root <root@blushingwound.aeza.network>
2025-11-16 16:49:09 -0500

    Reorganize: Move all app files to BuilderScan folder, clean up gitignore

--- .cursor-server/data/User/History/55d51163/dkQi.py
+++
@@ -1,6 +0,0 @@
-"""Kalshi Whale Bot - Monitor and tweet large trades on Kalshi."""
-
-__version__ = "1.0.0"
-__author__ = "Kalshi Whale Bot"
-__description__ = "A bot that monitors Kalshi for whale trades and posts them to X (Twitter)"
-
--- .cursor-server/data/User/History/77f4d0b1/sFDA.py
+++
@@ -1,6 +0,0 @@
-"""Valshi-X - Monitor and tweet large trades on Kalshi."""
-
-__version__ = "1.0.0"
-__author__ = "Valshi-X"
-__description__ = "A bot that monitors Kalshi for whale trades and posts them to X (Twitter)"
-
